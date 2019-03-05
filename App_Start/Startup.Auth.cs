9fc9fe3a724e575d55ff97f7a0f213166b2684ab
Josh Frawley <j.frawley@2016.ljmu.ac.uk>
2019-03-05 23:48:33 +0000

    Add files via upload

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace App_Start
+{
+    public partial class Startup1
+    {   
+        //This is getting all the infromation form the Office365 tenant
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+        private static string authority = aadInstance + tenantId;
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+               //set the defualt login to cookie based so users aren't logging in automaticly.
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            //If their is no cookie it will add one. 
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            //auths a user by their tenant
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri
+                });
+        }
+    }
+}
---
+++ App_Start/Startup1.cs
@@ -0,0 +1,61 @@
+﻿//All the libarys we need to run the code
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.Cookies;
+using System.Configuration;
+using System.Globalization;
+
+[assembly: OwinStartup(typeof(KeyTalkExample.App_Start.Startup1))]
+
+
+
+
+namespace KeyTalkExample.App_Start
+{
+    public class Startup1
+    {
+           //deauflt code that will request the tennant infromation form the Azure to auth the application can be run
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        string authority = string.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+
+        public void ConfigAuth(IAppBuilder app)
+        {
+            //This is setting the default auth as cookie so they wont have to log in each time
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            //This is getting the API information form the tenant in web.config
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error/message=" + context.Exception.Message);
+
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+
+        }
+    }
+}
