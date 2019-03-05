44c4b2edb74dfaf47b89b96c05b697a62e6e6f3a
Josh Frawley <j.frawley@2016.ljmu.ac.uk>
2019-03-05 23:50:44 +0000

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace App_Start
+{
+    public partial class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
